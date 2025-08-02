723c53b4534d8287a125e6c8ab7e0855335cd25d
Masayuki <rabo.hohoemi@gmail.com>
2025-08-02 11:21:46 +0900

    ♻️ export defaultの削除

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
-import Links from '../components/Links'
-import Headline from '../components/Healine'
+import { Links } from '../components/Links'
+import { Headline } from '../components/Healine'
 
 export default function Home() {
   return (
