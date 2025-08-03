5fe5cbf2c3bc7deb18a38178393ecfa87ef0d50a
Masayuki <rabo.hohoemi@gmail.com>
2025-08-03 10:36:50 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import classes from './Main.module.css'
-import { Links } from '../components/Links'
-import { Headline } from '../components/Healine'
+import classes from 'src/components/Main/Main.module.css'
+import { Headline } from 'src/components/Headline'
+import { Links } from 'src/components/Links'
 
 export function Main(props) {
   return (
