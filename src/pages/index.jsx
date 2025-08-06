70e4faab8c7f3feccd9b86fcd62a5450d4c9194a
Masayuki <rabo.hohoemi@gmail.com>
2025-08-06 18:03:41 +0900

    ✨ 講座用のuseEfectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import styles from 'src/styles/Home.module.css'
-import { Footer } from 'src/components/Footer'
-import { Main } from 'src/components/Main'
-import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
-
+import Head from 'next/head';
+import styles from 'src/styles/Home.module.css';
+import { Footer } from 'src/components/Footer';
+import { Main } from 'src/components/Main';
+import { Header } from 'src/components/Header';
+import { useCallback, useEffect } from 'react';
 
 // const handleClick = (e, foo) => {
 //   console.log(e.target.href);
@@ -21,6 +20,15 @@ export default function Home() {
     alert(foo);
   }, []);
 
+  useEffect(() => {
+    console.log('マウント時');
+    document.body.style.backgroundColor = 'lightblue';
+
+    return () => {
+      console.log('アンマウント時');
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -33,8 +41,8 @@ export default function Home() {
         ボタン
       </a>
 
-      <Main page="index" />
+      <Main page='index' />
       <Footer />
     </div>
-  )
+  );
 }
